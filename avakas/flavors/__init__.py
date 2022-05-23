13c6e4df4d5222201cd80ce6a3c2f3e1777cb9ee
Tyler Jachetta <tjachetta@imgix.com>
2022-05-23 06:48:37 -0700

    Removed erlang references, erl-terms and some dependencies

--- avakas/flavors/__init__.py
+++ avakas/flavors/__init__.py
@@ -5,14 +5,12 @@ Avakas Built-In Project Flavors
 from .base import AvakasLegacy
 from .ansible import AvakasAnsibleProject
 from .chef import AvakasChefProject
-from .erlang import AvakasErlangProject
 from .git import AvakasGitNative
 from .node import AvakasNodeProject
 
 __all__ = [
     'AvakasAnsibleProject',
     'AvakasChefProject',
-    'AvakasErlangProject',
     'AvakasGitNative',
     'AvakasLegacy',
     'AvakasNodeProject',
