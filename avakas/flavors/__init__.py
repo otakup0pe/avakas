2f17b5ea155d2669cf5acf0819b27fe79509c551
Jonathan Freedman <jonafree@gmail.com>
2026-02-24 13:54:17 -0800

    dang it there is another one

--- avakas/flavors/__init__.py
+++ avakas/flavors/__init__.py
@@ -6,10 +6,12 @@ from avakas.flavors.base import AvakasLegacy
 from avakas.flavors.ansible import AvakasAnsibleProject
 from avakas.flavors.git import AvakasGitNative
 from avakas.flavors.node import AvakasNodeProject
+from avakas.flavors.pep621 import AvakasP621Project
 
 __all__ = [
     'AvakasAnsibleProject',
     'AvakasGitNative',
     'AvakasLegacy',
     'AvakasNodeProject',
+    'AvakasP621Project',
 ]
